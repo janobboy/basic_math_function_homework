4bcd8eafb4a8d260307a3aaf09305c2b38ab1743
janobboy <qulamatovjanob@gmail.com>
2023-10-25 15:10:29 +0500

    code

--- basic_func14.py
+++ basic_func14.py
@@ -1,11 +1,4 @@
 def main(a, b):
-    '''find the floor division of a and b and return it.
-    
-    Args:
-        a (int): a number
-        b (int): a number
-        
-    Returns:
-        int: the result.
-    '''
-    return 
+    x=a//b 
+    return x
+print(main(11, 2))
