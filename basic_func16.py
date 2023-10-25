3050a97983599743848d82c21fc09e344ed85325
janobboy <qulamatovjanob@gmail.com>
2023-10-25 15:41:42 +0500

    code

--- basic_func16.py
+++ basic_func16.py
@@ -1,11 +1,4 @@
 def main(a, b):
-    '''find the sum of a and b and return it.
-    
-    Args:
-        a (int): a number
-        b (int): a number
-        
-    Returns:
-        int: the sum.
-    '''
-    return
+   x=a+b
+   return (x)
+print(main(3, 2))
