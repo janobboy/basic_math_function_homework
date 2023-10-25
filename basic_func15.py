19ba94ad1e1d1d131d71ad2495c7ecb4187a7f13
janobboy <qulamatovjanob@gmail.com>
2023-10-25 15:36:44 +0500

    code

--- basic_func15.py
+++ basic_func15.py
@@ -1,11 +1,4 @@
 def main(a, b):
-    '''Find the remainder when a is divided by b and return it.
-    
-    Args:
-        a (int): a number
-        b (int): a number
-        
-    Returns:
-        int: the result.
-    '''
-    return
+    x=a%b
+    return (x)
+print(main(23 , 5))
