8c5d78ea706662eccf429624bb1785c7e63a6fdb
janobboy <qulamatovjanob@gmail.com>
2023-10-25 15:03:35 +0500

    code

--- basic_func13.py
+++ basic_func13.py
@@ -1,10 +1,4 @@
 def main(a):
-    '''find the square root of a number and return it.
-    
-    Args:
-        a (int): a number
-        
-    Returns:
-        float: the absolute value.
-    '''
-    return
+   
+    return a
+print(main(9//3))
